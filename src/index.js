80984d891dd0fd1bc73af949f22910aaaa3b4041
Dominic Sherman <dsherman@hy-vee.com>
2019-08-23 11:31:30 -0500

    Fix it

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, withRouter} from 'react-router-dom';
-import {pdfjs} from 'react-pdf';
 
 import './css/index.css';
 
 import App from './App';
 
 const AppWithRouter = withRouter(App);
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 ReactDOM.render(
     <Router>
